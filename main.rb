3eb9ab8b6dead41f94dc4b9f0128cdfd568bc55c
Tony Contreras <tony.contreras@gmail.com>
2013-02-07 17:07:51 -0500

    skeleton done

--- main.rb
+++ main.rb
@@ -7,3 +7,34 @@ require 'pg'
 get '/' do
   erb :home
 end
+
+
+get '/add' do
+  @first = params[:first]
+  @last = params[:last]
+  @gender = params[:gender]
+  @image = params[:image]
+  @twitter = params[:twitter]
+  @github = params[:github]
+
+  sql = "insert into friends (first, last, gender, image, twitter, github) values ('#{@first}','#{@last}','#{@gender}','#{@image}','#{@twitter}','#{@github}');"
+
+  conn = PG.connect(:dbname =>'friends', :host => 'localhost')
+  conn.exec(sql)
+  conn.close
+
+  erb :add
+
+end
+
+
+get '/view' do
+  sql = "select * from friends;"
+  conn = PG.connect(:dbname =>'friends', :host => 'localhost')
+  @rows = conn.exec(sql)
+  conn.close
+
+  erb: view
+
+end
+
